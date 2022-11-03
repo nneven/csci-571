2b1403062a7ea82b4520b782b65d5b4c18f15f7e
niconeven <nneven@usc.edu>
2022-11-02 21:46:52 -0700

    start hw-8 search table

--- homework-8/app/src/pages/Search.js
+++ homework-8/app/src/pages/Search.js
@@ -1,6 +1,7 @@
 import './Search.css'
 import React, { useState } from 'react'
 import { Row, Col, Form, Button, Table } from 'react-bootstrap'
+import axios from 'axios'
 
 export default function Search() {
   const [keyword, setKeyword] = useState('')
@@ -10,8 +11,60 @@ export default function Search() {
   const [autoDetect, setAutoDetect] = useState(false)
   const [searchResult, setSearchResult] = useState([])
 
-  function submit(event) {
-    return alert(keyword + distance + category + location + autoDetect)
+  async function submit(event) {
+    event.preventDefault()
+
+    let latitude, longitude
+    if (autoDetect) {
+      await axios.get('https://ipinfo.io?token=41eab4d12ac318')
+        .then(response => {
+          const loc = response.data.loc.split(',')
+          latitude = loc[0]
+          longitude = loc[1]
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    } else {
+      await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyDZQNW6Ut1G3ySELQPBUsI6JpdatAUyxvo`)
+        .then(response => {
+          console.log(response)
+          const loc = response.data.results[0].geometry.location
+          latitude = String(loc.lat)
+          longitude = String(loc.lng)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+
+    let request = `term=${keyword}&latitude=${latitude}&longitude=${longitude}&category=${category}`
+    if (distance) request += `&radius=${parseInt(distance) * 1600}`
+    await axios.get('https://csci-571-363723.wl.r.appspot.com/yelp', {
+      params: {
+        url: `https://api.yelp.com/v3/businesses/search?${request}`
+      }
+    })
+      .then(response => {
+        if (response.data.error || response.data.businesses.length === 0) return
+        else {
+          const result = response.data.businesses.map(business => {
+            return {
+              name: business.name,
+              image: business.image_url,
+              rating: business.rating,
+              reviewCount: business.review_count,
+              address: business.location.display_address.join(', '),
+              phone: business.display_phone,
+              categories: business.categories.map(category => category.title).join(', '),
+              url: business.url
+            }
+          })
+          setSearchResult(result)
+          console.log(result)
+        }
+      }
+    )
   }
 
   function clear(event) {
@@ -73,7 +126,7 @@ export default function Search() {
       </Row>
       <Form.Group className="mb-3">
         <Form.Label>Location<span className="star">*</span></Form.Label>
-        <Form.Control type="text" value={location} onChange={e => setLocation(e.target.value)} required/>
+        <Form.Control type="text" value={location} onChange={e => setLocation(e.target.value)} required disabled={autoDetect}/>
       </Form.Group>
       <Form.Group className="mb-3">
         <Form.Check type="checkbox" label="Auto-detect my location" value={autoDetect} onChange={e => setAutoDetect(e.target.checked)} />
